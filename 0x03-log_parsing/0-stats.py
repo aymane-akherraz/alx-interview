cf684db1892ac0157e4c6f222e70b8b153dd9bbc
Aymane Akherraz <aymaneakherraz@gmail.com>
2024-07-16 23:33:23 +0100

    Modified 0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """ Reads stdin line by line and computes metrics """
 import sys
-import re
 
 
 def print_metrics(f_size, st_dict):
@@ -16,29 +15,13 @@ def print_metrics(f_size, st_dict):
 my_dict = {str(code): 0 for code in [200, 301, 400, 401, 403, 404, 405, 500]}
 total_file_size = 0
 i = 0
-pattern = (
-    r"([0-9]{1,3}\.){3}[0-9]{1,3}"
-    r"\s-\s"
-    r"\["
-    r"[0-9]{4}-[0-9]{2}-[0-9]{2}"
-    r"\s"
-    r"([0-9]{2}:){2}[0-9]{2}"
-    r"\.[0-9]{6}"
-    r"\]"
-    r'\s"GET\s/projects/260\sHTTP/1\.1"'
-    r"\s"
-    r"(?P<status_code>200|301|400|401|403|404|405|500)"
-    r"\s"
-    r"(?P<file_size>[0-9]{1,4})"
-)
 try:
     for line in sys.stdin:
         line = line.strip('\n')
-        matched = re.fullmatch(pattern, line)
         try:
-            file_size = int(matched.group('file_size'))
+            file_size = int(line.split()[-1])
             total_file_size += file_size
-            status_code = matched.group('status_code')
+            status_code = line.split()[-2]
             if status_code in my_dict.keys():
                 my_dict[status_code] += 1
         except Exception:
