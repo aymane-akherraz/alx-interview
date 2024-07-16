aed54f51770a2304957497970f174b69e5569424
Aymane Akherraz <aymaneakherraz@gmail.com>
2024-07-16 16:39:01 +0100

    Modified 0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,51 +1,35 @@
 #!/usr/bin/python3
 """ Reads stdin line by line and computes metrics """
 import sys
-import re
 
 
-def print_metrics():
+def print_metrics(f_size, status_dict):
     """ Print metrics to stdout """
-    print('File size: {}'.format(file_size))
-    sorted_keys = sorted(my_dict.keys())
+    print('File size: {}'.format(f_size))
+    sorted_keys = sorted(status_dict.keys())
     for k in sorted_keys:
-        print('{}: {}'.format(k, my_dict[k]))
+        if status_dict[k] > 0:
+            print('{}: {}'.format(k, status_dict[k]))
 
 
-my_dict = {}
+my_dict = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
 file_size = 0
 i = 0
-pattern = (
-    r"([0-9]{1,3}\.){3}[0-9]{1,3}"
-    r"\s-\s"
-    r"\["
-    r"[0-9]{4}-[0-9]{2}-[0-9]{2}"
-    r"\s"
-    r"([0-9]{2}:){2}[0-9]{2}"
-    r"\.[0-9]{6}"
-    r"\]"
-    r'\s"GET\s/projects/260\sHTTP/1\.1"'
-    r"\s"
-    r"(?P<status_code>200|301|400|401|403|404|405|500)"
-    r"\s"
-    r"(?P<file_size>[0-9]{1,4})"
-)
+
 try:
     for line in sys.stdin:
         line = line.strip('\n')
-        matched = re.fullmatch(pattern, line)
-        if matched:
-            file_size += int(matched.group('file_size'))
-            status_code = int(matched.group('status_code'))
+        splited = line.split()
+        if len(splited) == 7:
+            file_size += int(splited[-1])
+            status_code = int(splited[-2])
             if status_code in my_dict:
                 my_dict[status_code] += 1
-            else:
-                my_dict[status_code] = 1
             if i < 9:
                 i += 1
             else:
-                print_metrics()
+                print_metrics(file_size, my_dict)
                 i = 0
 except KeyboardInterrupt:
-    print_metrics()
+    print_metrics(file_size, my_dict)
     raise
