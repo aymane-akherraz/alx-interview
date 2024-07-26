5d975782e3a9f048d3f17a662603cd75814979f6
Aymane Akherraz <aymaneakherraz@gmail.com>
2024-07-26 16:05:40 +0100

    Modified some files

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -15,7 +15,7 @@ def validUTF8(data):
                 n_bytes = 1
             elif (byte >> 4) == 0b1110:
                 n_bytes = 2
-            elif (byte >> 5) == 0b11110:
+            elif (byte >> 3) == 0b11110:
                 n_bytes = 3
             else:
                 return False
