19d3daa13cc0367e8cf466950a8623675173d850
Aymane Akherraz <aymaneakherraz@gmail.com>
2024-08-20 11:23:01 +0100

    Created 0x08 and added some files

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" Making Change Problem """
+
+
+def makeChange(coins, total):
+    """ 
+    Determine the fewest number of coins needed
+    to meet a given amount total 
+    """
+    
+    if total <= 0:
+        return 0
+
+    coins.sort(reverse=True)
+    nb_coins = 0
+
+    for c in coins:
+        if c <= total:
+            while total - c >= 0:
+                total -= c
+                nb_coins += 1
+            if total == 0:
+                return nb_coins
+    
+    return -1
